2127df4ef31a1b9e7503f5dc3553732b49b53c0e
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2025-09-05 02:10:41 -0700

    Export get all functions too

--- src/just_scrape/wrappers/__init__.py
+++ src/just_scrape/wrappers/__init__.py
@@ -1,11 +1,13 @@
 from .get_buy_box_offers_ import get_buy_box_offers
 from .get_new_title_buckets_ import get_new_title_buckets
-from .get_new_titles_ import get_new_titles
-from .get_season_episodes_ import get_season_episodes
+from .get_new_titles_ import get_all_new_titles, get_new_titles
+from .get_season_episodes_ import get_all_season_episodes, get_season_episodes
 from .get_title_detail_article_ import get_title_detail_article
 from .get_url_title_details_ import get_url_title_details
 
 __all__ = [
+    "get_all_new_titles",
+    "get_all_season_episodes",
     "get_buy_box_offers",
     "get_new_title_buckets",
     "get_new_titles",
